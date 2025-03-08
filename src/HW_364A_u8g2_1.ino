3a51405adb37aff66f6d056729381fd2b7b57ed2
Kristian Wiklund <githubkristian@snabela.nl>
2025-03-08 19:37:42 +0100

    sending data to signalk!

--- src/HW_364A_u8g2_1.ino
+++ src/HW_364A_u8g2_1.ino
@@ -37,15 +37,36 @@ void handle_oled(int c) {
 #include "sensors/digital_input.h"
 #include "transforms/frequency.h"
 #include "signalk/signalk_output.h"
+#include "sensors/analog_input.h"
+
+float waterlevel=0.0;
+
+class FulSensor: public NumericSensor {
+
+ public:
+   FulSensor();
+
+   void set(float);
+
+ private:
+   float thevalue;
+
+};
+
+FulSensor::FulSensor() {
+}
+
+void FulSensor::set(float ap) {
+  thevalue=ap;
+  this->emit(ap);
+  }
+  
 
 ReactESP app([]() {
 
+
   u8g2.begin();
 
-  app.onRepeat(1000,[] () {
-  handle_oled(c);
-  c++;
-  });
 
 
 #ifndef SERIAL_DEBUG_DISABLED
@@ -54,52 +75,20 @@ ReactESP app([]() {
 
   sensesp_app = new SensESPApp();
 
-  // Everything above this line is boilerplate, except for the #include statements
-
-  // This is the beginning of the code to read the SHT31 temperature and humidity
-  // sensor.
-
-  auto* sht31 = new SHT31(0x45);
-  const uint sht31_read_delay = 1000;
-
-  auto* sht31_temperature =
-      new SHT31Value(sht31, SHT31Value::temperature, sht31_read_delay, "/fridge/temperature");
-  sht31_temperature->connect_to(
-      new SKOutputNumber("environment.inside.refrigerator.temperature"));
-
-  auto* sht31_humidity =
-      new SHT31Value(sht31, SHT31Value::humidity, sht31_read_delay, "/fridge/humidity");
-  sht31_humidity->connect_to(
-      new SKOutputNumber("environment.inside.refrigerator.humidity"));
-
-   // This is the end of the SHT31 code and the beginning of the RPM counter code. Notice
-   // that nothing special has to be done - just start writing the code for the next sensor.
-   // You'll notice slight differences in code styles - for example, the code above
-   // defines the config paths "in place", and the code below defines them with a variable,
-   // then uses the variable as the function parameter. Either approach works.  
-
-  const char* sk_path = "propulsion.main.revolutions";
-  const char* config_path_calibrate = "/sensors/engine_rpm/calibrate";
-  const char* config_path_skpath = "/sensors/engine_rpm/sk";
-  const float multiplier = 1.0 / 97.0;
-  const uint rpm_read_delay = 500;
-
-#ifdef ESP8266
-  uint8_t pin = D5;  
-#elif defined(ESP32)
-  uint8_t pin = 4;
-#endif
-  auto* dic = new DigitalInputCounter(pin, INPUT_PULLUP, RISING, rpm_read_delay);
-
-  dic
-      ->connect_to(new Frequency(multiplier, config_path_calibrate)) 
-      ->connect_to(new SKOutputNumber(sk_path, config_path_skpath));
-
-    // This is the end of the RPM counter code.
-
+FulSensor* water_level = new FulSensor(); 
+water_level->connect_to(	     
+   new SKOutputNumber("tanks.freshWater.0.currentLevel"));	
+	
     // If you wanted to add a third, fourth, or more sensor, you would do that
     // here. An ESP9266 should easily handle four or five sensors, and an ESP32
     // should handle eight or ten, or more.
-    
+
+  app.onRepeat(1000,[water_level] () {
+     handle_oled(c);
+     c++;
+     water_level->set(c);
+  });
+
+
   sensesp_app->enable();
 });
